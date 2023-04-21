25c3b76cde7581b4cce43aeb5d0292208894452d
BorisVassilev1 <37549016+BorisVassilev1@users.noreply.github.com>
2023-04-21 18:19:18 +0300

    Started work on native physics
    	- Just detect distances between objects for now.

--- src/engine.native.js
+++ src/engine.native.js
@@ -3,11 +3,16 @@
 //	Virtual Prosthetics 1.0
 //
 
-
-
+import * as THREE from "../libs/three.module.js";
 
 var scene, bodies;
 
+const BodyType = {
+	BALL:  0,
+	BOX:  1,
+	CONVEX: 2,
+	PLANE: 3,
+}
 
 function init( _scene, ground, _bodies )
 {
@@ -16,44 +21,74 @@ function init( _scene, ground, _bodies )
 	scene = _scene;
 	scene.physics = {}; // native physics (if needed)
 	
-	ground.physics = {};  // native physics (if needed)
+	ground.physics = {
+		type: BodyType.PLANE,
+		level: 0.
+	};  // native physics (if needed)
 	ground.physics.threejs = ground; // back link
 
 } // createNativeScene
 
 
+var _v = new THREE.Vector3;
 
 // animation cycle
 
 function update( fps, time, dTime )
 {
+	// world positions needed.
+	for( var body of bodies ) if( body.mainMesh )
+	{
+		body.mainMesh.getWorldPosition( _v );	
+		body.physics.position.copy( _v );
+	}
+
+	// do the math
 	for( var body of bodies )
 	{
 		// do something with the body
-		
-		// body.physics = physics data
-		// body.mainMesh = THREE.Object3D
-		
-		// body.beginContact( otherBody );
-		// body.endContact( otherBody );
+		let pos = body.physics.position;
+
+		for( let other of bodies ) {
+			if(other === body) continue;
+			let other_pos = other.physics.position;
+
+			// a very simple check to proove the system is working
+			let distance = pos.distanceTo(other_pos);
+			if(distance < 0.4) {
+				body.beginContact(other);
+			} else {
+				body.endContact(other);
+			}
+		}
 	}
 }
 
 
-function convex( vertices, faces )
+// TODO: later
+function convex( _vertices, _faces )
 {
 	// vertices = [ [x1, y1, z1], [x2, y2, z2], ... ]
 	// faces = [ [ccw vertex indices], [ccw vertex indices], ... ]
 	
-	var body = { }; // physics data
+	var body = { 
+		type: BodyType.CONVEX,
+		vertices: _vertices,
+		faces: _faces,
+		position: new THREE.Vector3(0,0,0),
+	}; // physics data
 	
 	return body;
 }
 
 
-function ball( radius )
+function ball( _radius )
 {
-	var body = { }; // physics data
+	var body = { 
+		type: BodyType.BALL,
+		radius: _radius,
+		position: new THREE.Vector3(0,0,0),
+	}; // physics data
 	
 	return body;
 }
@@ -61,7 +96,11 @@ function ball( radius )
 
 function box( sizex, sizey, sizez )
 {
-	var body = { }; // physics data
+	var body = {
+		type: BodyType.BOX,
+		size: new THREE.Vector3(sizex, sizey, sizez),
+		position: new THREE.Vector3(0,0,0),
+	}; // physics data
 	
 	return body;
 }
