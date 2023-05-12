219e741eaae105ac4fb1bd24b60e402153d622d1
boytchev <70705048+boytchev@users.noreply.github.com>
2023-05-12 19:37:44 +0300

    Refactored phalange GLTF code

--- assets/gltf/hand/REBUILD.bat
+++ assets/gltf/hand/REBUILD.bat
@@ -1,13 +1,18 @@
-call gltf-transform weld phalange4x1.glb step1.glb
-call gltf-transform simplify step1.glb step2.glb
-call gltf-transform simplify step2.glb step1.glb
-call gltf-transform simplify step1.glb step2.glb
-call gltf-transform simplify step2.glb step1.glb
-call gltf-transform simplify step1.glb step2.glb
-call gltf-transform simplify step2.glb step1.glb
-call gltf-transform simplify step1.glb phalange4x1.min.glb
-
-del step1.glb
-del step2.glb
+call :optimize phalange10x3, 2
+call :optimize phalange5x3, 2
 pause
+exit /b 0
+
+:optimize
+	@call gltf-transform weld %~1.glb step1.glb
+	for /L %%a in (1,1,%~2) do (
+		@call gltf-transform simplify step1.glb step2.glb --ratio 0.1 --error 0.001
+		@call gltf-transform simplify step2.glb step1.glb --ratio 0.1 --error 0.001
+	)
+	@del %~1.min.glb
+	@copy step1.glb %~1.min.glb
+	
+	del step1.glb
+	del step2.glb
+exit /b 0
 
---
+++ assets/gltf/hand/phalange.js
@@ -0,0 +1,29 @@
+﻿function createPhalange( )
+{
+	// main rectangular body
+	body = cube( [0,HEIGHT/2,0], [WIDTH, HEIGHT+2*WIDTH], 'gray' );
+	extraBody = cube( [0,HEIGHT/2,0], [WIDTH, HEIGHT+2*WIDTH, 0.25*WIDTH], 'gray' );
+
+	// curved polishing of body edges
+	curve = sphere( [0,HEIGHT/2,0], [WIDTH*1.3, HEIGHT+WIDTH*0.8] );
+
+	// axial openings for attaching other elemtns
+	axisUp = prism( 20, [0,HEIGHT,-WIDTH], [WIDTH*1.3/3,2*WIDTH] );
+		its.spinV = 90;
+	axisDown = axisUp.clone;
+		its.y = 0;
+
+	//flatUp = cube( [WIDTH/2,HEIGHT,0], [WIDTH,WIDTH,2*WIDTH], 'yellow' );
+	cutUp = prism( 30, [0.4*WIDTH,HEIGHT+0.5*WIDTH,-WIDTH], [2*WIDTH,2*WIDTH], 'yellow' );
+		its.spinV = 90;
+		
+	// circular cut at bottom
+	cutDown = prism( 10, [0.4*WIDTH,-0.5*WIDTH,-0.3*WIDTH/2], [2*WIDTH,0.3*WIDTH], 'yellow' );
+		its.spinV = 90;
+		
+	g = group(body,extraBody,curve,axisUp,axisDown,cutDown,cutUp);
+		its.visible = false;
+
+	phalange = construct( '( body - cutUp + extraBody - (axisUp + axisDown) )*curve - cutDown' );
+		its.color = 'white';
+}
